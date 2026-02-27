5306c98e31fff5c94d51cf58344298e42dd4d2d6
robicivan <robicivan99@gmail.com>
2026-02-27 13:48:04 +0100

    cleanup

--- PersonalDiary/Program.cs
+++ PersonalDiary/Program.cs
@@ -21,7 +21,6 @@ namespace PersonalDiary
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
